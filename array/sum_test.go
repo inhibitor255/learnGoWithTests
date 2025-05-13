e43d06e05a389c23179fbd1bc6224f51e0e4b4f2
inhibitor255 <kaungmyatkyaw255@gmail.com>
2025-05-14 03:49:05 +0630

    test(SumAllTail function): added test of sum all tail function

--- array/sum_test.go
+++ array/sum_test.go
@@ -23,6 +23,27 @@ func TestSumAll(t *testing.T) {
 	got := SumAll([]int{1, 2, 3}, []int{0, 4})
 	want := []int{6, 4}
 
+	checkSums(t, got, want)
+}
+
+func TestSumAllTail(t *testing.T) {
+	t.Run("with not empty array", func(t *testing.T) {
+		got := SumAllTail([]int{1, 2, 4}, []int{3, 3})
+		want := []int{6, 3}
+
+		checkSums(t, got, want)
+	})
+
+	t.Run("with empty array", func(t *testing.T) {
+		got := SumAllTail([]int{1, 2, 4}, []int{3, 3}, []int{})
+		want := []int{6, 3, 0}
+
+		checkSums(t, got, want)
+	})
+}
+
+func checkSums(t *testing.T, got, want []int) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("want %v but got %v", want, got)
 	}
